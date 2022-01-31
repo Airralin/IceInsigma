ef42acd5ccd7d9bb4a3907a0ff390473f5729de4
Airralin <alkranz67946@gmail.com>
2022-01-30 21:42:04 -0600

    Petes problem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,37 +23,16 @@ public class Main {
         final int MED = 500;
         final int FAST = 100;
 
-        JFrame frame = new JFrame();
-        JButton[][] button = new JButton[ROWS][COLS];
-        JPanel panel = new JPanel();
         //JLabel[] status = new JLabel[COLS];
         //images
         ImageIcon lord = new ImageIcon("src/character-icons/Lord.png");
-        GridLayout grid;
-        grid = new GridLayout( ROWS,COLS );
-        frame.setLayout( grid );
         //
         Map map = new Map(ROWS, COLS);
         Character Chrom = new Character(1,22,7,6,3,8,false,lord);
         map.placeCharacter(Chrom, 11,3);
         //
         //Create JButtons initialize, activate listeners,
-        for (int r=0; r < button.length; r++)
-        {
-            for(int c=0; c < button[r].length; c++)
-            {
-
-                button[r][c] = new JButton();
-
-                button[r][c].setBorderPainted(true);
-                Character temp = map.getCharacterAt(r,c);
-                if(temp != null)
-                {
-                    button[r][c].setIcon(temp.getPic());
-                }
-                frame.add(button[r][c]);
-            }
-        }
+        JFrame frame = map.getMapFrame();
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setVisible(true);
     }
--- src/com/company/Map.java
+++ src/com/company/Map.java
@@ -1,30 +1,54 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Map {
     private Tile[][] map;
+    private JButton buttons[][];
+    private GridLayout grid;
+    private JFrame mapFrame;
+
 
     private Map(){}
     public Map(int rows, int cols)
     {
         map = new Tile[rows][cols];
+        buttons = new JButton[rows][cols];
+        grid = new GridLayout(rows, cols);
+        mapFrame = new JFrame();
+        mapFrame.setLayout(grid);
+
         for(int r = 0; r < rows; r++)
         {
             for(int c = 0; c < cols; c++)
             {
                 map[r][c] = new Tile();
+                buttons[r][c] = new JButton();
+                buttons[r][c].setBorderPainted(true);
+                mapFrame.add(buttons[r][c]);
             }
         }
     }
 
     public boolean placeCharacter(Character character, int row, int col)
     {
-        return map[row][col].placeCharacter(character);
+        if(map[row][col].placeCharacter(character))
+        {
+            buttons[row][col].setIcon(character.getPic());
+            return true;
+        }
+        return false;
     }
 
     public Character getCharacterAt(int row, int col)
     {
         return map[row][col].getCharacter();
+    }
+
+    public JFrame getMapFrame()
+    {
+        return mapFrame;
     }
 }
